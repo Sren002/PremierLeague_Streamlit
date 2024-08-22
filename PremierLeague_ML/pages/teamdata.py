76df8f21c4601f1960c14b2a9df25ea750a6a7f5
Sren002 <sethren2002@gmail.com>
2024-08-21 19:26:43 -0600

    starting data analysis
    taking data for each year and separating it into "W", "L", and "D" rows. Next step is visualizing the percentage of wins each specific team got per year in a bar chart.

--- PremierLeague_ML/pages/teamdata.py
+++ PremierLeague_ML/pages/teamdata.py
@@ -1,12 +1,57 @@
 import streamlit as st 
 import pandas as pd
+from PIL import Image
+import requests
+from io import BytesIO
+import matplotlib as plt
 
-st.sidebar.page_link('streamlit_app.py', label='Home')
-st.sidebar.page_link('pages/teams.py', label='Teams')
+
+# Loading Image using PIL
+url = 'https://github.com/Sren002/PremierLeague_Streamlit/blob/main/PremierLeague_ML/resources/premier_league_logo.png?raw=true'
+response = requests.get(url)
+im = Image.open(BytesIO(response.content))
 
 df = pd.read_csv("https://raw.githubusercontent.com/Sren002/web-scraping-PremierLeagueML/main/matches.csv")
 
 dfML = pd.read_csv("https://raw.githubusercontent.com/Sren002/web-scraping-PremierLeagueML/main/predictions.csv")
 
 team = st.session_state["team"]
+
+# configue page
+st.set_page_config(
+    page_title=("%s Data Analysis" % team),
+    layout = 'wide',
+    initial_sidebar_state = 'expanded',
+    page_icon = im)
+
+st.logo(im)
+st.html("""
+  <style>
+    [alt=Logo] {
+      height: 6rem;
+    }
+  </style>
+        """)
+
+st.sidebar.page_link('streamlit_app.py', label='Home')
+st.sidebar.page_link('pages/teams.py', label='Teams')
+
+
+
+data = df.loc[df['Team'] == "%s" % (team)]
+
+for i in range(2024, 2020, -1):
+    date = data.loc[(data["Date"].astype(str).str[:4]) == str(i)]
+    
+    wins = date.loc[date["Result"] == "W"]
+    draws = date.loc[date["Result"] == "D"]
+    losses = date.loc[date["Result"] == "L"]
+
+    winsNum = len(wins.index)
+    drawsNum = len(draws.index)
+    lossesNum = len(losses.index)
+    total = winsNum + drawsNum + lossesNum
+
+    st.title("%s" % str(i))
+
 
--- PremierLeague_ML/pages/teams.py
+++ PremierLeague_ML/pages/teams.py
@@ -1,6 +1,31 @@
 import streamlit as st
 import pandas as pd
 import math
+from PIL import Image
+import requests
+from io import BytesIO
+
+
+# Loading Image using PIL
+url = 'https://github.com/Sren002/PremierLeague_Streamlit/blob/main/PremierLeague_ML/resources/premier_league_logo.png?raw=true'
+response = requests.get(url)
+im = Image.open(BytesIO(response.content))
+
+# configue page
+st.set_page_config(
+    page_title='Teams',
+    layout = 'wide',
+    initial_sidebar_state = 'expanded',
+    page_icon = im)
+
+#st.logo(im)
+st.html("""
+  <style>
+    [alt=Logo] {
+      height: 5rem;
+    }
+  </style>
+        """)
 
 df = pd.read_csv("https://raw.githubusercontent.com/Sren002/web-scraping-PremierLeagueML/main/matches.csv")
 
